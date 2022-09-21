34a55764555f667ded16f3452abc4b6a2d1104d4
Kartheek-247 <107993742+Kartheek-247@users.noreply.github.com>
2022-09-21 16:35:55 +0530

    Update settings.py

--- newproj/settings.py
+++ newproj/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'newproj.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   #}
 }
 
 
